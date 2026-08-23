a6f09f073feada9bf7aad33ae2ceaec8136019f6
PedroDGR96 <pedro.goes8083@gmail.com>
2026-08-23 15:54:06 -0300

    Loop patch 741: auto-gated patch [model loop]

--- app/verticals/foodtruck/models/order_item_addon.rb
+++ app/verticals/foodtruck/models/order_item_addon.rb
@@ -18,3 +18,25 @@ class OrderItemAddon < ApplicationRecord
     order_item&.refresh_line_total!
   end
 end
+
+class AddonGroup < ApplicationRecord
+  include BusinessScoped
+  include TenantChild
+
+  belongs_to :order_item, optional: true
+  belongs_to :product_addon_group, optional: true
+
+  has_many :order_item_addons, dependent: :destroy
+
+  validates :name, presence: true
+  validates_parent_business_for :order_item, :product_addon_group
+
+  after_save :refresh_parent
+  after_destroy :refresh_parent
+
+  private
+
+  def refresh_parent
+    order_item&.refresh_line_total!
+  end
+end
