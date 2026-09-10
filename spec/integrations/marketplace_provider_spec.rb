180eaacbf560c8f559c98e33e052bfc24afb1540
PedroDGR96 <pedro.goes8083@gmail.com>
2026-09-10 18:26:59 -0300

    Loop patch 962: auto-gated patch [model loop]

--- spec/integrations/marketplace_provider_spec.rb
+++ spec/integrations/marketplace_provider_spec.rb
@@ -1,10 +1,12 @@
 require "rails_helper"
 
 RSpec.describe MarketplaceProvider do
-  it { expect { described_class.create_order(settings: {}, args: {}) }.to raise_error(NotImplementedError) }
-  it { expect { described_class.test_connection(settings: {}) }.to raise_error(NotImplementedError) }
-  it { expect { described_class.update_order(settings: {}, args: {}) }.to raise_error(NotImplementedError) }
-  it { expect { described_class.cancel_order(settings: {}, args: {}) }.to raise_error(NotImplementedError) }
-  it { expect { described_class.status(settings: {}, args: {}) }.to raise_error(NotImplementedError) }
-  it { expect { described_class.webhook_verify(settings: {}, args: {}) }.to raise_error(NotImplementedError) }
+  describe "class methods" do
+    it { expect { described_class.create_order(settings: {}, args: {}) }.to raise_error(NotImplementedError) }
+    it { expect { described_class.test_connection(settings: {}) }.to raise_error(NotImplementedError) }
+    it { expect { described_class.update_order(settings: {}, args: {}) }.to raise_error(NotImplementedError) }
+    it { expect { described_class.cancel_order(settings: {}, args: {}) }.to raise_error(NotImplementedError) }
+    it { expect { described_class.status(settings: {}, args: {}) }.to raise_error(NotImplementedError) }
+    it { expect { described_class.webhook_verify(settings: {}, args: {}) }.to raise_error(NotImplementedError) }
+  end
 end
