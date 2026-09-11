9a185bd116803bdaf8b488c195e53a6d24c34c33
PedroDGR96 <pedro.goes8083@gmail.com>
2026-09-11 00:13:38 -0300

    Loop patch 1063: auto-gated patch [model loop]

---
+++ spec/policies/daily_report_policy_spec.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+RSpec.describe DailyReportPolicy do
+  let(:business) { create(:business) }
+  let(:owner) { Tenancy.with_business(business) { create(:user, :owner, business: business) } }
+  let(:cashier) { Tenancy.with_business(business) { create(:user, :cashier, business: business) } }
+  let(:kitchen) { Tenancy.with_business(business) { create(:user, :kitchen, business: business) } }
+
+  def policy_for(user, record = Object)
+    described_class.new(user, record)
+  end
+
+  describe "#show?" do
+    it "returns true for owner" do
+      expect(policy_for(owner).show?).to be(true)
+    end
+
+    it "returns false for cashier" do
+      expect(policy_for(cashier).show?).to be(false)
+    end
+
+    it "returns false for kitchen" do
+      expect(policy_for(kitchen).show?).to be(false)
+    end
+  end
+end
